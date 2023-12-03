695eca334a394d7bb006a7bb220f2c5dadb129fd
Victória Olívia Araújo Vilas Boas <victoriaovilas+github@gmail.com>
2023-12-02 22:35:35 -0300

    remove front end

--- backend/internal/core/ports/ports.go
+++ backend/internal/core/ports/ports.go
@@ -8,6 +8,12 @@ import (
 
 // quando inserimos receitas os ingredientes são populados automaticamente
 type RepositoryProvider interface {
+	UpsertGrocery(ctx context.Context, grocery domain.Grocery) error
+	DeleteGrocery(ctx context.Context, externalID string) error
+	InsertRecipe(ctx context.Context, recipe domain.Recipe) error
+	ListGroceries(ctx context.Context, filter GroceryFilter) ([]domain.Grocery, error)
+	ListRecipes(ctx context.Context, filter RecipeFilter) ([]domain.Recipe, error)
+	InsertIngredients(ctx context.Context, ingredient domain.Ingredient) error
 }
 
 type HomeCookingServiceProvider interface {
--- backend/internal/repository/model.go
+++ backend/internal/repository/model.go
@@ -1,42 +1,44 @@
 package repository
 
-import "time"
+import (
+	"time"
+)
 
 type Recipe struct {
-	ID         int
-	ExternalID string
-	Name       string
-	Yield      int32
-	CookTime   time.Duration
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID         int           `json:"id" db:"id"`
+	ExternalID string        `json:"external_id" db:"external_id"`
+	Name       string        `json:"name" db:"name"`
+	Yield      int32         `json:"yield" db:"yield"`
+	CookTime   time.Duration `json:"cook_time" db:"cook_time"`
+	CreatedAt  time.Time     `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time     `json:"updated_at" db:"updated_at"`
 }
 
 type Ingredient struct {
-	ID         int
-	ExternalID string
-	Name       string
+	ID         int    `json:"id" db:"id"`
+	ExternalID string `json:"external_id" db:"external_id"`
+	Name       string `json:"name" db:"name"`
 }
 
 type Grocery struct {
-	ID            int
-	ExternalID    string
-	Name          string
-	Ingredient    string
-	Type          string
-	PurchaseDate  time.Time
-	DueDate       time.Time
-	IsPerishable  bool
-	QuantityValue float64
-	QuantityUnit  string
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-	Available     bool
+	ID            int       `json:"id" db:"id"`
+	ExternalID    string    `json:"external_id" db:"external_id"`
+	Name          string    `json:"name" db:"name"`
+	Ingredient    string    `json:"ingredient" db:"ingredient"`
+	Type          string    `json:"type" db:"type"`
+	PurchaseDate  time.Time `json:"purchase_date" db:"purchase_date"`
+	DueDate       time.Time `json:"due_date" db:"due_date"`
+	IsPerishable  bool      `json:"is_perishable" db:"is_perishable"`
+	QuantityValue float64   `json:"quantity_value" db:"quantity_value"`
+	QuantityUnit  string    `json:"quantity_unit" db:"quantity_unit"`
+	CreatedAt     time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at" db:"updated_at"`
+	Available     bool      `json:"available" db:"available"`
 }
 
 type RecipeIngredient struct {
-	RecipeID      int
-	IngredientID  int
-	QuantityValue float64
-	QuantityUnit  string
+	RecipeID      int     `json:"recipe_id" db:"recipe_id"`
+	IngredientID  int     `json:"ingredient_id" db:"ingredient_id"`
+	QuantityValue float64 `json:"quantity_value" db:"quantity_value"`
+	QuantityUnit  string  `json:"quantity_unit" db:"quantity_unit"`
 }
